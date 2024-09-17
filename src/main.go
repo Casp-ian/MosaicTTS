8484b4424386e04e09ddc4283941b048598d9949
caspian <caspian@koolwaaij.nl>
2024-09-17 22:42:15 +0200

    parse whisper-timestamped output data to linked list like structure

--- src/main.go
+++ src/main.go
@@ -1,17 +1,54 @@
 package main
 
 import "fmt"
+import "github.com/spf13/cobra"
+
+type LyricsListEnty struct {
+	Song  string
+	Start float32
+	End   float32
+	Text  string
+	Next  *LyricsListEnty
+}
 
 func main() {
-    // read user input string
-    fmt.Println("Hello, World!");
+	var rootCmd = &cobra.Command{
+		Use:   "<string>",
+		Short: "input a string, as output a audio file of the given string will be output",
+		// Long: `A Fast and Flexible Static Site Generator built with
+		//                love by spf13 and friends in Go.
+		//                Complete documentation is available at http://hugo.spf13.com`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO handle error
+			test(args[0])
+		},
+	}
 
-    // parse lyrics files
-    Parse();
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		// os.Exit(1)
+	} // read user input string
+}
 
-    // calculate which splices of songs we want
+func test(input string) {
+	fmt.Println("starting...", input)
 
-    // splice and concatenate these
+	// parse lyrics files
+	var lyricsList LyricsListEnty
+	lyricsList = Parse()
 
-    // output sound file
+	// var test *LyricsListEnty = &lyricsList
+	// for test != nil {
+	// 	fmt.Println(test.Text)
+	// 	test = test.Next
+	// }
+
+	// calculate which splices of songs we want
+	// spliceList := decideSplices(lyricsList)
+
+	// splice and concatenate these
+	// doFFMPEGMagic(spliceList)
+
+	fmt.Println("Done!")
+	// output sound file
 }
--- src/parse.go
+++ src/parse.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-func Parse() {
-    fmt.Println("Hello, DOG!");
+type WhisperTimestampedJson struct {
+	Text     string                      `json:"name"`
+	Segments []WhisperTimestampedSegment `json:"segments"`
+}
+
+type WhisperTimestampedSegment struct {
+	Text       string                    `json:"text"`
+	Start      float32                   `json:"start"`
+	End        float32                   `json:"end"`
+	Confidence float32                   `json:"confidence"`
+	Words      []WhipserTimestampedWords `json:"words"`
+}
+
+type WhipserTimestampedWords struct {
+	Text       string  `json:"text"`
+	Start      float32 `json:"start"`
+	End        float32 `json:"end"`
+	Confidence float32 `json:"confidence"`
+}
+
+func Parse() LyricsListEnty {
+	var fileName = "../lyrics/hello.json"
+
+	jsonFile, err := os.Open(fileName)
+	defer jsonFile.Close()
+
+	if err != nil {
+		fmt.Printf("cant open %v...\n", fileName)
+	}
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Printf("invalid json in %v...\n", fileName)
+	}
+
+	var jsonData WhisperTimestampedJson
+	json.Unmarshal(byteValue, &jsonData)
+
+	var head *LyricsListEnty
+
+	// filling this shit backwards, so when walking through the linked list it will be in order
+	for i := len(jsonData.Segments) - 1; i >= 0; i-- {
+		for j := len(jsonData.Segments[i].Words) - 1; j >= 0; j-- {
+			head = &LyricsListEnty{
+				fileName,                            // Song  string
+				jsonData.Segments[i].Words[j].Start, // Start float32
+				jsonData.Segments[i].Words[j].End,   // End   float32
+				jsonData.Segments[i].Words[j].Text,  // Text  string
+				head,                                // Next  *LyricsListEnty
+			}
+		}
+	}
+	return *head
 }
